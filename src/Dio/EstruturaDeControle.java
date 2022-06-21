fb8a9a964e7bbcbc5612ca107161bb8c18af4d53
JonathasGit <jonathas-25@hotmail.com>
2022-06-21 11:41:31 -0300

    Continuando os testes

--- src/Dio/EstruturaDeControle.java
+++ src/Dio/EstruturaDeControle.java
@@ -1,36 +1,45 @@
 package Dio;
 
 public class EstruturaDeControle {
-    public static void main(String[]args) {
-
+    public static void main(String[] args) {
 
         ifFlecha();
         int idade = 19;
-       
 
-
-        if(idade > 18){
+        if (idade > 18) {
             System.out.println("Maior de Idade");
         }
-    
-       
-       
-        
-        
+
     }
 
     private static void ifFlecha() {
-        int mes = 2;
+        int mes = 3;
 
        
         if(mes == 1){
             System.out.println("Janeiro");
         } else{ if(mes == 2 ){
             System.out.println("Fevereiro");
+        } else{ if(mes == 3){
+            System.out.println("Março");
+        } else{ if(mes == 4){
+            System.out.println("Abril");
+        } else{ if(mes == 5){
+            System.out.println("Maio");
+        }else{ if(mes == 6){
+            System.out.println("Junho");
+        }
+
+        }
+
+        }
+
+        }
+
         }
 
         }
         
     }
-    
+
 }
