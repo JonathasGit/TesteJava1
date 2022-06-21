abc26479232b22079dda46b0cc8eada4ab756d67
JonathasGit <jonathas-25@hotmail.com>
2022-06-20 21:21:30 -0300

    Testes java

---
+++ src/teste1.java
@@ -0,0 +1,12 @@
+package teste;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        for(int i=9, g=1; i<10; g++, i++, System.out.println("Será que funciona ?")) {
+
+        }
+
+    }
+}
---
+++ src/teste2.java
@@ -0,0 +1,32 @@
+public class teste2 {
+    
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano1";
+        String s3 = "String3";
+        boolean b1 = true; 
+        boolean b2 = false;
+
+        long l1 = 199L;
+        long l2 = 899l;
+        byte y1 = 1;
+        short h1 = 25; 
+
+        System.out.println("il == i2" + " " + (i1 == i2)); //Vai retonar falso
+        System.out.println("il != i2" + " " + (i1 != i2)); //Vai retonar verdadeiro 
+        System.out.println("il > i2" + " " + (i1 > i2)); //Vai retonar falso
+        System.out.println("il <= i2" + " " + (i1 <= i2)); //Vai retonar verdadeiro
+
+
+
+
+
+    }
+}
