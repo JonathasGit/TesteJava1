a89776c3cf18dcc09a03b6014c8cdcb77573f8d8
JonathasGit <jonathas-25@hotmail.com>
2022-06-22 19:01:53 -0300

    #3 Estrutura de dados em Java - Vetores

---
+++ src/EstruturaDeDados/DadosJava.java
@@ -0,0 +1,30 @@
+package EstruturaDeDados;
+
+// Aqui vamos fazer um Vetor = Estrutura de dados 
+
+public class DadosJava {
+    public static void main(String[] args) {
+
+        //Declarando um vetor
+        int[] vetor = new int[10];
+        //INserindo dados no vetor
+        vetor[0] = 3;
+        vetor[1] = 5;
+        vetor[2] = 10;
+        vetor[3] = 15;
+        vetor[4] = 21;
+        vetor[5] = 33;
+        vetor[6] = 41;
+        vetor[7] = 12;
+        vetor[8] = 88;
+        vetor[9] = 332;
+        System.out.println(vetor[9]);
+
+
+
+        int x = 10;
+        System.out.println("Valor de x: " + x);
+        x = 4;
+        System.out.println("Valor de x novamente: " + x);
+    }
+}
